c8eaef19df279900751918509f4970bc9578a381
NIregbulem <soccergirl0000123@gmail.com>
2019-02-28 00:44:09 -0800

    Create BinarySearch.py

---
+++ BinarySearch.py
@@ -0,0 +1,21 @@
+
+def binary_Search(num_list, num):
+  var mid_val;
+  low_val = 0;
+  high_val = len(num_list) - 1;
+  result = False;
+  while (high_val >= low_val not result):
+        mid_val = (high_val + low_val)/2;
+        if (num_list[mid_val] == num :
+        result = True;
+        else:
+            if(num < num_list[mid_val]:
+            high_val = mid_val - 1;
+            else:
+                low_val = mid_val + 1;
+  return result
+  
+  print(binary_Search([1,3,5,7,9], 4))
+  print(binary_Search([2,4,6,8,10], 6))
+            
+  
