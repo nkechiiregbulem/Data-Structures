1ca22bb92d97c1cabe7dfee8a900d3f0cfc095bd
NIregbulem <soccergirl0000123@gmail.com>
2019-03-01 11:55:40 -0800

    Create Rand_Val(JSON).js

---
+++ Rand_Val(JSON).js
@@ -0,0 +1,24 @@
+How to get random values in JSON
+
+var jsonContent= {
+          "featured": [
+            {
+              "id": "111",
+              "title": "post title 111",
+              "desc": "This is a test desc 111"
+            },
+            {
+              "id": "222",
+              "title": "post title 222",
+              "desc": "This is a test desc 222"
+            },
+            {
+              "id": "333",
+              "title": "post title 333",
+              "desc": "This is a test desc 333"
+            }
+    ]
+};
+
+var random = jsonContent["featured"][Math.floor(Math.random()*jsonContent["featured"].length)];
+console.log(random)
